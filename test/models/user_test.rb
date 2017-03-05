7219bb5ee32b81f7d7fd8fe6d0bc4ff6c9cb33b2
minhthuan274 <doanminhthuan274@gmail.com>
2017-03-05 21:20:13 +0700

    Error test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -36,7 +36,7 @@ class UserTest < ActiveSupport::TestCase
 
   test "email validation should accept valid addresses" do 
   	valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
-                         first.last@foo.jp alice+bob@baz.cn]
+                         first.last@f+oo.jp alice+bob@baz.cn]
     valid_addresses.each do |valid_address|
       @user.email = valid_address
       assert @user.valid?, "#{valid_address.inspect} should be valid"
