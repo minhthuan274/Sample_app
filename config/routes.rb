fa235ae1b029b6de00b47341d6e276633fe26de0
minhthuan274 <doanminhthuan274@gmail.com>
2017-03-06 21:01:44 +0700

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/show'
+
+  get 'users/new'
+
 	root 'static_pages#home'
   get '/about',   to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/help',    to: 'static_pages#help'
-	get '/signup',  to: 'users#new'  
+	get '/signup',  to: 'users#new'
+	post '/signup', to: 'users#create'
+	resources :users  
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,5 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get signup_path
-    assert_response :success
-  end
 
 end
