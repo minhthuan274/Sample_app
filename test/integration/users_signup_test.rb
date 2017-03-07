4228ea9d5c1d992d4fde0b9aac48a6629991dffa
minhthuan274 <doanminhthuan274@gmail.com>
2017-03-07 17:43:18 +0700

    Convert tabs to spaces

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,27 +1,27 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-	test "invalid signup information" do 
-		get signup_path
-		assert_no_difference 'User.count' do 
-			post users_path, params: { user: { name: "",
-																				 email: "user@invalid",
-																				 password:              "foo",
-																				 password_confirmation: "foo"}}
-		end
-		assert_template 'users/new'
-	end
+   test "invalid signup information" do 
+      get signup_path
+      assert_no_difference 'User.count' do 
+         post users_path, params: { user: { name: "",
+                                            email: "user@invalid",
+                                            password:              "foo",
+                                            password_confirmation: "foo"}}
+      end
+      assert_template 'users/new'
+   end
 
-	test "valid signup infromation" do 
-		get signup_path
-		assert_difference 'User.count', 1 do 
-			post users_path, params: { user: { name:  "Example User",
-																			   email: "user@example.com",
-																			   password:              "thuan274",
-																			   password_confirmation: "thuan274" } }
-		end	
-		follow_redirect!
-		assert_template 'users/show'
-		assert_not_nil(flash)
-	end
+   test "valid signup infromation" do 
+      get signup_path
+      assert_difference 'User.count', 1 do 
+         post users_path, params: { user: { name:  "Example User",
+                                            email: "user@example.com",
+                                            password:              "thuan274",
+                                            password_confirmation: "thuan274" } }
+      end   
+      follow_redirect!
+      assert_template 'users/show'
+      assert_not_nil(flash)
+   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,27 +6,27 @@ class UserTest < ActiveSupport::TestCase
   # end
 
   def setup
-  	@user = User.new(name: "Example User", email: "user@example.com",
+    @user = User.new(name: "Example User", email: "user@example.com",
                      password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do 
-  	assert @user.valid?
+    assert @user.valid?
   end
 
   test "name should be present" do 
-  	@user.name = " ";
-  	assert_not @user.valid?
+    @user.name = " ";
+    assert_not @user.valid?
   end
 
   test "email should be present" do 
-  	@user.email = " ";
-  	assert_not @user.valid?
+    @user.email = " ";
+    assert_not @user.valid?
   end
 
   test "name should not be too long" do 
-  	@user.name = "a" * 51
-  	assert_not @user.valid?
+    @user.name = "a" * 51
+    assert_not @user.valid?
   end
 
   test "email should not be too long" do
@@ -35,7 +35,7 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "email validation should accept valid addresses" do 
-  	valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
                          first.last@foo.jp alice+bob@baz.cn]
     valid_addresses.each do |valid_address|
       @user.email = valid_address
@@ -53,26 +53,26 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "email should be unique" do 
-  	duplicate_user = @user.dup
-  	duplicate_user.email = @user.email.upcase
-  	@user.save
-  	assert_not duplicate_user.valid?
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
   end
 
   test "user's email should be saved as lower case" do 
-  	mixed_case_email = "Foo@ExAMPle.CoM"
-  	@user.email = mixed_case_email
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
     @user.save
     assert_equal mixed_case_email.downcase, @user.reload.email
   end
 
   test "password should be present (non blank)" do 
-  	@user.password = @user.password_confirmation = " " * 5
-  	assert_not @user.valid?
+    @user.password = @user.password_confirmation = " " * 5
+    assert_not @user.valid?
   end
 
   test "password should have a minimum length" do 
-  	@user.password = @user.password_confirmation = "a" * 3
-  	assert_not @user.valid?
+    @user.password = @user.password_confirmation = "a" * 3
+    assert_not @user.valid?
   end
 end
