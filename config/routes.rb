472ad282baec3ae6d4522722832e2fefa5a1f399
minhthuan274 <doanminhthuan274@gmail.com>
2017-03-07 17:53:02 +0700

    Convert tabs to spaces

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
 
   get 'users/new'
 
-	root 'static_pages#home'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/help',    to: 'static_pages#help'
-	get  '/signup',  to: 'users#new'
-	post '/signup',  to: 'users#create'
-	resources :users  
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+  root 'static_pages#home'
+  resources :users  
 end
