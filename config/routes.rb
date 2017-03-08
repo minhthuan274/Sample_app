23124c1811dff6370143a16859a9ee7f592bf9a0
minhthuan274 <doanminhthuan274@gmail.com>
2017-03-08 09:15:00 +0700

    Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/show'
-
-  get 'users/new'
-
-  get  '/about',   to: 'static_pages#about'
-  get  '/contact', to: 'static_pages#contact'
-  get  '/help',    to: 'static_pages#help'
-  get  '/signup',  to: 'users#new'
-  post '/signup',  to: 'users#create'
+  get    '/about',   to: 'static_pages#about'
+  get    '/contact', to: 'static_pages#contact'
+  get    '/help',    to: 'static_pages#help'
+  get    '/signup',  to: 'users#new'
+  post   '/signup',  to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   root 'static_pages#home'
   resources :users  
 end
---
+++ test/integration/users_login_test.rb
@@ -0,0 +1,52 @@
+require 'test_helper'
+
+class UsersLoginTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:thuan)
+  end
+
+  test "login with invalid information" do 
+    #visit the login page and verify that new session form render properly
+    get login_path
+    assert_template 'sessions/new'
+    # Post to the session path with an invalid params 
+    # and verify that the new session form get re-render and the flash message appears
+    post login_path, params: { session: { email: "", password: "" } }
+    assert_template 'sessions/new' 
+    assert_not flash.empty?
+
+    # Visit another page like Home page
+    # Verify the flash doesn't appear on that page 
+    get root_path
+    assert flash.empty?
+  end
+
+  test "login with valid information followed by logout" do 
+    # Visit the login path 
+    get login_path 
+    # Post valid information user to login path 
+    post login_path, params: { session: { email: @user.email,
+                                          password: 'password' } }
+    assert is_logged_in?
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show' 
+    # Verify the login path disappears
+    assert_select "a[href=?]", login_path, count: 0
+    # Verify the logout path appears
+    assert_select "a[href=?]", logout_path 
+    # Verify the profile link appears
+    assert_select "a[href=?]", user_path(@user)
+
+    # Log out 
+    delete logout_path 
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path,      count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
+  end
+
+end
