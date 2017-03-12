49f637ef7b740118ff3c1dad227e14b6d4ba0728
minhthuan274 <doanminhthuan274@gmail.com>
2017-03-12 21:10:57 +0700

    config production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<your heroku app>.herokuapp.com'
+  host = 'sample-app-minhthuan274.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
