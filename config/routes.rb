8b87e0cdf706bdf17224b4ba541bbca978a15c58
minhthuan274 <doanminhthuan274@gmail.com>
2017-03-12 10:42:59 +0700

    Add account activation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root   'static_pages#home'
   get    '/about',   to: 'static_pages#about'
   get    '/contact', to: 'static_pages#contact'
   get    '/help',    to: 'static_pages#help'
@@ -7,6 +8,6 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-  root 'static_pages#home'
   resources :users  
+  resources :account_activations, only: [:edit]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,9 @@ User.create!(name:  "Minh Thuan",
              email: "doanminhthuan274@gmail.com",
              password:              "thuan274",
              password_confirmation: "thuan274",
-             admin: true)
+             admin:     true,
+             activated: true,
+             activated_at: Time.zone.now)
 
 99.times do |n|
   name  = Faker::Name.name
@@ -11,5 +13,7 @@ User.create!(name:  "Minh Thuan",
   User.create!(name:  name,
                email: email,
                password:              password,
-               password_confirmation: password)
+               password_confirmation: password,
+               activated: true,
+               activated_at: Time.zone.now)
 end
